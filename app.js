82dc5d2a6dda85094acc9c1a3bf4b285ff3fcc71
Patrycja <patcho2808@gmail.com>
2020-09-03 12:16:48 +0200

    adding recipes to db

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const db = require('monk')('localhost/recipe_picker');
 const app = express(); 
 const port = 3666;
 
+app.use(express.urlencoded({extended: false}));
 app.use('/css', express.static('./node_modules/bootstrap/dist/css'));
 app.use('/js', express.static('./node_modules/bootstrap/dist/js'));
 app.use(express.static('./public/'));
@@ -38,5 +39,18 @@ app.get('/', async (req, res) => {
     
     res.render('index', {recipes}); 
 }); 
+
+
+app.get('/add', (req, res) => {
+    res.render('addRecipe', {message: false}); 
+})
+
+app.post('/add', async (req, res) => {
+    console.log(req.body.name); 
+    const recipes_collection = db.get('recipes'); 
+    const ingredients = req.body.ingredients.split(', '); 
+    await recipes_collection.insert({name: req.body.name, ingredients, link: req.body.link });
+    res.render('addRecipe', {message: `Recipe "${req.body.name}" added succesfully`}); 
+})
 
 app.listen(port, () => console.log(`Listening on: http://localhost:${port}`)); 
