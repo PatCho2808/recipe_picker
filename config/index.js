b8bbc43ba565c7a18adf39708c73a73654ef141d
Patrycja <patcho2808@gmail.com>
2020-09-06 11:58:09 +0200

    Added .env

--- config/index.js
+++ config/index.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-
 module.exports = {
   endpoint: process.env.API_URL,
   port: process.env.PORT
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
-const app = require('./app');  
+const app = require('./app');
+const { port } = require('./config/index');
 
-app.listen(process.env.PORT || 3000, console.log(`listening on http://localhost:${port}`)); 
+app.listen(process.env.PORT || port); 
