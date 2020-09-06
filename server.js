2309b2f6fad81a6b48ce438ee6835f6286bab68b
Patrycja <patcho2808@gmail.com>
2020-09-06 11:48:56 +0200

    Added process.env.PORT to app.listen

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require('./app'); 
 const { port } = require('./config/index.js'); 
 
-app.listen(port, console.log(`listening on http://localhost:${port}`)); 
+app.listen(process.env.PORT || port, console.log(`listening on http://localhost:${port}`)); 
