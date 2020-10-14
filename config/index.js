eb618de44d6d3a68acc7ec895a3f33c806f426cd
Patrycja <patcho2808@gmail.com>
2020-10-14 07:52:00 +0200

    fixed .env file

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 module.exports = {
   host: process.env.HOST,
-  port: process.env.PORT, 
-  mongodb_url = process.env.MONGODB_URL, 
+  port: process.env.PORT,
+  mongodb_url: process.env.MONGODB_URL,
 };
