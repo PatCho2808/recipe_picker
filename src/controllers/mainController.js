4886f7ad6684754108be7d39328ac134cbca8e73
Patrycja <patcho2808@gmail.com>
2021-04-01 10:52:38 +0200

    Style list

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,45 +1,45 @@
-const { validationResult } = require("express-validator");
-const mongoService = require("../services/mongoService");
-const searchService = require("../services/searchService");
-const scrapService = require("../services/scrapingService");
+const { validationResult } = require('express-validator');
+const mongoService = require('../services/mongoService');
+const searchService = require('../services/searchService');
+const scrapService = require('../services/scrapingService');
 
 module.exports = {
-  createRecipe: async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      console.error(errors.array());
-      res.render("addRecipe", { message: "Wrong values", recipe: {} });
-      return;
-    }
-    const ingredients = req.body.ingredients.split(", ");
-    await mongoService.addRecipe(req.body.title, ingredients, req.body.link);
-    res.render("addRecipe", {
-      message: `Recipe "${req.body.title}" added succesfully`,
-      recipe: {},
-    });
-  },
+	createRecipe: async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			console.error(errors.array());
+			res.render('addRecipe', { message: 'Wrong values', recipe: {} });
+			return;
+		}
+		const ingredients = req.body.ingredients.split(', ');
+		await mongoService.addRecipe(req.body.title, ingredients, req.body.link);
+		res.render('addRecipe', {
+			message: `Recipe "${req.body.title}" added succesfully`,
+			recipe: {}
+		});
+	},
 
-  searchForRecipes: async (req, res) => {
-    let recipes = [];
-    if (req.query.ingredients) {
-      const ingredients = req.query.ingredients.split(" ");
-      recipes = await searchService.getRecipesSortedByMatchingIngredients(
-        ingredients
-      );
-    }
-    res.render("index", { recipes });
-  },
+	searchForRecipes: async (req, res) => {
+		let recipes = [];
+		if (req.query.ingredients) {
+			const ingredients = req.query.ingredients.split(' ');
+			recipes = await searchService.getRecipesSortedByMatchingIngredients(
+				ingredients
+			);
+		}
+		res.render('index', { recipes });
+	},
 
-  scrapeRecipe: async (req, res) => {
-    try {
-      const url = req.body.scrap_link;
-      const { title, ingredients } = await scrapService.getRecipeFromPage(url);
-      res.render("addRecipe", {
-        message: "",
-        recipe: { title, ingredients, link: url },
-      });
-    } catch (error) {
-      res.render("addRecipe", { message: "Wrong url", recipe: {} });
-    }
-  },
+	scrapeRecipe: async (req, res) => {
+		try {
+			const url = req.body.scrap_link;
+			const { title, ingredients } = await scrapService.getRecipeFromPage(url);
+			res.render('addRecipe', {
+				message: '',
+				recipe: { title, ingredients, link: url }
+			});
+		} catch (error) {
+			res.render('addRecipe', { message: 'Wrong url', recipe: {} });
+		}
+	}
 };
