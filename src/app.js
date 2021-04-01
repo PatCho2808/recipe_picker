bef5a330535304f678bf5f96afabd789e57c3d06
Patrycja <patcho2808@gmail.com>
2021-04-01 10:01:18 +0200

    Styled content

--- src/app.js
+++ src/app.js
@@ -4,23 +4,20 @@ const helmet = require('helmet');
 
 const app = express();
 
-app.use(compression());
-app.use(helmet());
-app.use(
-	helmet.contentSecurityPolicy({
-		directives: {
-			defaultSrc: ["'self'"],
-			scriptSrc: ["'unsafe-inline'", "'self'"]
-		}
-	})
-);
+// app.use(compression());
+// app.use(helmet());
+// app.use(
+// 	helmet.contentSecurityPolicy({
+// 		directives: {
+// 			defaultSrc: ["'self'"],
+// 			scriptSrc: ["'unsafe-inline'", "'self'"]
+// 		}
+// 	})
+// );
 
 const routes = require('./routes/routes');
 
 app.use(express.urlencoded({ extended: false }));
-// app.use("/css", express.static("./node_modules/bootstrap/dist/css"));
-// app.use("/js", express.static("./node_modules/bootstrap/dist/js"));
-// app.use("/js", express.static("./node_modules/jquery/dist"));
 app.use(express.static('./public/'));
 app.use(routes);
 
