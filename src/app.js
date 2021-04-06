b8318c6196b9bf4b1836927aff8e6d69d6d8e5b7
Patrycja <patcho2808@gmail.com>
2021-04-06 08:40:16 +0200

    Added csp that ionicons work with

--- src/app.js
+++ src/app.js
@@ -4,16 +4,25 @@ const helmet = require('helmet');
 
 const app = express();
 
-// app.use(compression());
-// app.use(helmet());
-// app.use(
-// 	helmet.contentSecurityPolicy({
-// 		directives: {
-// 			defaultSrc: ["'self'"],
-// 			scriptSrc: ["'unsafe-inline'", "'self'"]
-// 		}
-// 	})
-// );
+app.use(compression());
+app.use(helmet());
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: ["'self'"],
+			scriptSrc: [
+				"'unsafe-inline'",
+				"'self'",
+				'https://unpkg.com/',
+				'http://localhost:3000/f8181f8a-24be-4d94-a0f7-aa9024a20373',
+				'blob:'
+			],
+			styleSrc: ['https://fonts.googleapis.com', "'self'", "'unsafe-inline'"],
+			fontSrc: ['https://fonts.gstatic.com/', "'self'"],
+			connectSrc: ['https://unpkg.com']
+		}
+	})
+);
 
 const routes = require('./routes/routes');
 
